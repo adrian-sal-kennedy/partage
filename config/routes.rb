4fd88235a68a3547be9178d1a2aad03f9618fb1c
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-14 23:30:15 +1000

    add custom fonts, index stubs, logo

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'businesses/create'
   get 'businesses/update'
   get 'businesses/destroy'
+  get 'businesses', to: 'businesses#index'
   root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
