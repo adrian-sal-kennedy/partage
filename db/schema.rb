8f7553ad0213c9633065fb75898df5065c264a69
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-15 16:33:39 +1000

    add devise user, set it up. probs broken at this point

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_161001) do
+ActiveRecord::Schema.define(version: 2020_05_15_063249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,14 @@ ActiveRecord::Schema.define(version: 2020_05_14_161001) do
     t.bigint "business_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["business_id"], name: "index_users_on_business_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "agreements", "businesses"
