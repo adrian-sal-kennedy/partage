0fd99eee527327c5168e4b4a0f4c3599b9fed1cf
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-20 12:25:58 +1000

    fixes

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
     resources :resources
     resources :agreements
   end
-  resources :agreements
-  resources :resources
+  resources :resources do
+    resources :agreements
+  end
   root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
