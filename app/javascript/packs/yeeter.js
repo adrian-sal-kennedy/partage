62a56b0fc3ef705d874525266ee5d3a0c9ab1282
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-05-26 04:29:27 +1000

    added intro pages

--- app/javascript/packs/yeeter.js
+++
@@ -1,8 +0,0 @@
-yeet = function yeet(classToYeet,yeetClass) {
-  var elements = document.getElementsByClassName(classToYeet);
-  var blk = Array.prototype.filter.call(elements, function(element){
-    element.classList.remove(yeetClass);
-    void element.offsetWidth;
-    element.classList.add(yeetClass);
-  });
-}
